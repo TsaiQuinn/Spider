f311739f84945010615f75d333ff75545a7ade71
TsaiQuinn <yiqiqulvyou@gmail.com>
2017-08-23 18:00:47 +0800

    采集车品牌相关数据

---
+++ SpiderCommon/SpiderConfig.cs
@@ -0,0 +1,13 @@
+﻿#region ----------------备注----------------
+// Author:CK 
+// FileName:SpiderConfig.cs 
+// Create Date:2017-08-22
+// Create Time:16:04 
+#endregion
+namespace SpiderCommon
+{
+    public class SpiderConfig
+    {
+        
+    }
+}
---
+++ SpiderCommon/SpiderRequest.cs
@@ -0,0 +1,13 @@
+﻿#region ----------------备注----------------
+// Author:CK 
+// FileName:SpiderRequest.cs 
+// Create Date:2017-08-22
+// Create Time:15:29 
+#endregion
+namespace SpiderCommon
+{
+    public class SpiderRequest
+    {
+        
+    }
+}
