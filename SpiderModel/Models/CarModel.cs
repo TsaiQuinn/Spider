41491b94c85090bcb63f925bf76510d7055917a2
TsaiQuinn <yiqiqulvyou@gmail.com>
2017-08-30 17:51:55 +0800

    添加车系采集的部分代码

---
+++ SpiderModel/Models/CarModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpiderModel.Models
+{
+    class CarModel
+    {
+        /// <summary>
+        ///     主键ID
+        /// </summary>
+        public virtual int? Id { get; set; }
+
+        /// <summary>
+        /// 品牌ID
+        /// </summary>
+        public virtual int? BrandId { get; set; }
+
+        /// <summary>
+        ///     车系名称
+        /// </summary>
+        public virtual string ModelName { get; set; }
+
+        /// <summary>
+        ///     车系链接地址
+        /// </summary>
+        public virtual string LinkUrl { get; set; }
+
+        /// <summary>
+        ///     厂家
+        /// </summary>
+        public virtual string Factory { get; set; }
+
+        /// <summary>
+        ///     品牌TAG
+        /// </summary>
+        public virtual string TagName { get; set; }
+
+        /// <summary>
+        ///     添加时间
+        /// </summary>
+        public virtual DateTime? AddTime { get; set; }
+    }
+}
--- SpiderPresenters/CrawlPresenter.cs
+++ SpiderPresenters/CrawlPresenter.cs
@@ -46,8 +46,8 @@ namespace SpiderPresenters
         {
             View.Load += OnViewLoad;
             View.PickBrandEvent += OnPickBrand;
-            View.PickModelEvent += OnPickModel;   
-        } 
+            View.PickModelEvent += OnPickModel;
+        }
 
         /// <summary>
         ///     采集品牌
@@ -62,12 +62,20 @@ namespace SpiderPresenters
                 var task = SpiderRequest.DownloadString(url);
                 task.Wait();
                 string brandStr = task.Result.Substring(14, task.Result.Length - 15);
-                JObject objJObject = (JObject)JsonConvert.DeserializeObject(brandStr);
+                JObject objJObject = (JObject) JsonConvert.DeserializeObject(brandStr);
                 Dictionary<string, object> resultDictionary =
                     JsonConvert.DeserializeObject<Dictionary<string, object>>(objJObject["brand"].ToString());
                 var definition = new[]
                 {
-                    new {type = string.Empty, id = 0, name = string.Empty, url = string.Empty, cur = string.Empty, num = 0}
+                    new
+                    {
+                        type = string.Empty,
+                        id = 0,
+                        name = string.Empty,
+                        url = string.Empty,
+                        cur = string.Empty,
+                        num = 0
+                    }
                 };
                 IList<CarBrand> listBrands = (from valuePair in resultDictionary
                         let result = JsonConvert.DeserializeAnonymousType(valuePair.Value.ToString(), definition)
@@ -87,14 +95,70 @@ namespace SpiderPresenters
                 if (listBrands.Count > 0)
                 {
                     CarBrandBusiness.ShowInfoEvent += CarBrandBusiness_ShowDataEvent;
-                    CarBrandBusiness.SaveOrUpdateList(listBrands);
-                    CarBrandBusiness.ShowInfoEvent -= CarBrandBusiness_ShowDataEvent;
+                    var parallelResult = Parallel.ForEach(listBrands, carBrand =>
+                    {
+                        var modelTask =
+                            SpiderRequest.DownloadString(
+                                $"{ConfigurationManager.AppSettings["BrandsUrl"]}&pagetype=masterbrand&objid={carBrand.Rid}");
+                        modelTask.Wait();
+                        string modelStr = modelTask.Result.Substring(14, modelTask.Result.Length - 15);
+                        JObject modelJObject = (JObject) JsonConvert.DeserializeObject(modelStr);
+                        Dictionary<string, object> modelDictionary =
+                            JsonConvert.DeserializeObject<Dictionary<string, object>>(modelJObject["brand"].ToString());
+                        var tempModel = new[]
+                            {new {type = "", name = "", url = "", butie = "", salestate = "", cur = ""}};
+                        object pair = modelDictionary[carBrand.TagName];
+                        JArray array = JArray.FromObject(pair);
+
+//                        from token in JArray.FromObject(pair)
+//                        where token["child"]!=null
+//                        let name = token["name"].ToString()
+//                        let jToken = JToken.FromObject(token["child"])
+//                        from child in jToken
+//                        where child["child"] != null
+//                        let childArray = JArray.FromObject(child["child"])
+//                        from model in childArray
+//                        select new 
+
+                        foreach (JToken token in array)
+                        {
+                            if (token["child"] != null)
+                            {
+                                string name = token["name"].ToString();
+                                JToken jToken = JToken.FromObject(token["child"]);
+                                foreach (JToken child in jToken)
+                                {
+                                    if (child["child"] != null) //二级
+                                    {
+                                        JArray childArray = JArray.FromObject(child["child"]);
+
+//                                        JToken childToken = JToken.FromObject(child["child"]);
+//                                        foreach (
+//                                            ECarSeries carModel in
+//                                            childToken.Select(
+//                                                token1 => new ECarSeries(0, carBrand.Id, token1["name"].ToString(),
+//                                                    _domain + token1["url"], null, child["name"].ToString(), 0)))
+//                                        {
+//                                            bll.Insert(carModel);
+//                                            j++;
+//                                            bll.InsertAction?.Invoke(carModel, j);
+//                                        }
+                                    }
+                                }
+                                break;
+                            } 
+                        }
+                    });
+                    if (parallelResult.IsCompleted)
+                    {
+                        CarBrandBusiness.ShowInfoEvent -= CarBrandBusiness_ShowDataEvent;
+                    }
                 }
                 else
                 {
                     View.ShowMessage("没有获取到品牌", "提示");
                 }
-            }); 
+            });
         }
 
         /// <summary>
@@ -114,8 +178,6 @@ namespace SpiderPresenters
         /// <param name="e"></param>
         private void OnViewLoad(object sender, EventArgs e)
         {
-//            var list = CarBrandBusiness.QueryList(null);
-//            View.ShowMessage($"品牌数量为{list.Count}", "采集品牌"); 
         }
 
         /// <summary>
